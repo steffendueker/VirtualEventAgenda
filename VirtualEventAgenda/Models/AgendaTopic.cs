f5855f2e446e8639710c5e1a354efec82821bda1
Niels Ophey <niophey@microsoft.com>
2020-09-02 17:11:44 +0200

    Build the inital new web project

---
+++ VirtualEventAgenda/Models/AgendaTopic.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace WebApp.Models
+{
+    public class AgendaTopic
+    {
+        public string Title { get; set; }
+        public int OrderId { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string TeamsUrl { get; set; }
+        public string PresentationUrl { get; set; }
+    
+    }
+}
